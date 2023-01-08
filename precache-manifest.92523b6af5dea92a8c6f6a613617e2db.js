8f2e28609fe64568bb977925a274d8580e9ed904
dhungk39 <dhungk39@gmail.com>
2023-01-08 07:02:30 -0800

    v12

---
+++ precache-manifest.92523b6af5dea92a8c6f6a613617e2db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fe50ee7ff25ec88d2c74e1294860e79",
+    "url": "/mycv/index.html"
+  },
+  {
+    "revision": "8e0c463639c2fc3839b1",
+    "url": "/mycv/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8b46f21a134687927c8c",
+    "url": "/mycv/static/js/2.11bda28e.chunk.js"
+  },
+  {
+    "revision": "4472b9b1c403804b7335229d4dbf4deb",
+    "url": "/mycv/static/js/2.11bda28e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e0c463639c2fc3839b1",
+    "url": "/mycv/static/js/main.b801cdbb.chunk.js"
+  },
+  {
+    "revision": "9c9156fde34d30785013",
+    "url": "/mycv/static/js/runtime-main.f7b5f11b.js"
+  }
+]);

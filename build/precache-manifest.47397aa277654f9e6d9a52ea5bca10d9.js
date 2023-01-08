7c34ef03222bbb7b6717bc941399731a0da82f78
dhungk39 <dhungk39@gmail.com>
2023-01-08 05:09:07 -0800

    define image

---
+++ build/precache-manifest.47397aa277654f9e6d9a52ea5bca10d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e5fe84386480867a4c9ec4e95ef62d6",
+    "url": "/mycv/index.html"
+  },
+  {
+    "revision": "1a7e76acfa5cbd1fc664",
+    "url": "/mycv/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8b46f21a134687927c8c",
+    "url": "/mycv/static/js/2.11bda28e.chunk.js"
+  },
+  {
+    "revision": "4472b9b1c403804b7335229d4dbf4deb",
+    "url": "/mycv/static/js/2.11bda28e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a7e76acfa5cbd1fc664",
+    "url": "/mycv/static/js/main.e2c16dfb.chunk.js"
+  },
+  {
+    "revision": "9c9156fde34d30785013",
+    "url": "/mycv/static/js/runtime-main.f7b5f11b.js"
+  }
+]);
